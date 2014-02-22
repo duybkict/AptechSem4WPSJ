d2e73ae6606ebe5856b6d61f640d8e17ecbf10ef
Duy Trần <duytran.bkict@gmail.com>
2014-02-22 12:03:28 +0700

    Update

--- database/script.sql
+++ database/script.sql
@@ -270,7 +270,7 @@ VALUES ('Chocolate cookies - Love candle cake - Chocolate leaf',
 		2, 2, 100.0);
 
 -- users
-INSERT INTO users(email, password, full_name) VALUE ('admin@email.com', 'admin', 'admin');
+INSERT INTO users(email, password, full_name, type) VALUE ('admin@email.com', 'admin', 'admin', 2);
 
 -- SELECT * FROM articles LIMIT 2 OFFSET 4
 -- SELECT * FROM feedback
